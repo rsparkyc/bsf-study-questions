ac2a9a5ca02e0948a7f470e666e0f3ba8598079a
Ryan Caskey <casker@gmail.com>
2023-11-07 16:56:27 -0500

    formatting, but also fixes https://github.com/rsparkyc/bsf-study-questions/issues/28

--- src/components/auth/LoginPage.tsx
+++ src/components/auth/LoginPage.tsx
@@ -1,54 +1,56 @@
-import './LoginPage.css';
+import "./LoginPage.css";
 
-import { AccessToken, AuthContextHolder } from '../../api/bsf/AuthContext';
-import React, { useCallback, useEffect, useState } from 'react';
+import { AccessToken, AuthContextHolder } from "../../api/bsf/AuthContext";
+import React, { useCallback, useEffect, useState } from "react";
 
-import { AuthorizeRequest } from '../../api/bsf/requests/auth/AuthorizeRequest';
-import { ConfigurationRequest } from '../../api/bsf/requests/auth/ConfigurationRequest';
-import { ConfirmedRequest } from '../../api/bsf/requests/auth/ConfirmedRequest';
-import { PersonRequest } from '../../api/bsf/requests/PersonRequest';
-import { RefreshTokenRequest } from '../../api/bsf/requests/auth/RefreshTokenRequest';
-import { SelfAssertedRequest } from '../../api/bsf/requests/auth/SelfAssertedRequest';
-import SettingsComponent from '../SettingsComponent';
-import { TokenRequest } from '../../api/bsf/requests/auth/TokenRequest';
+import { AuthorizeRequest } from "../../api/bsf/requests/auth/AuthorizeRequest";
+import { ConfigurationRequest } from "../../api/bsf/requests/auth/ConfigurationRequest";
+import { ConfirmedRequest } from "../../api/bsf/requests/auth/ConfirmedRequest";
+import { PersonRequest } from "../../api/bsf/requests/PersonRequest";
+import { RefreshTokenRequest } from "../../api/bsf/requests/auth/RefreshTokenRequest";
+import { SelfAssertedRequest } from "../../api/bsf/requests/auth/SelfAssertedRequest";
+import SettingsComponent from "../SettingsComponent";
+import { TokenRequest } from "../../api/bsf/requests/auth/TokenRequest";
 
 interface LoginPageProps {
-  onLoginStateChange: (loggedIn: boolean) => void;
+    onLoginStateChange: (loggedIn: boolean) => void;
 }
 
 const LoginPage: React.FC<LoginPageProps> = ({ onLoginStateChange }) => {
-
-    const [email, setEmail] = useState<string>('');
-    const [password, setPassword] = useState<string>('');
+    const [email, setEmail] = useState<string>("");
+    const [password, setPassword] = useState<string>("");
     const [isLoggedIn, setIsLoggedIn] = useState<boolean>(false);
-    const [user, setUser] = useState<string>('');
+    const [user, setUser] = useState<string>("");
     const [accessToken, setAccessToken] = useState<AccessToken | null>(null);
 
     const [devMode, setDevMode] = useState<boolean>(false);
-    const [tokenTimeLeft, setTokenTimeLeft] = useState<number>(accessToken?.expires_in || 0);
+    const [tokenTimeLeft, setTokenTimeLeft] = useState<number>(
+        accessToken?.expires_in || 0
+    );
 
-    const [isDrawerOpen, setIsDrawerOpen] = useState(false);
+    const [isDisclaimerOpen, setIsDisclaimerOpen] = useState(false);
 
     const [isLoggingIn, setIsLoggingIn] = useState(false);
     const [error, setError] = useState<null | string>(null);
 
-
     // Check if already logged in
-    const decodeJWT = (token: string): {given_name: string, family_name: string} | null => {
+    const decodeJWT = (
+        token: string
+    ): { given_name: string; family_name: string } | null => {
         try {
             // Get the payload, which is the second part of the token
-            const base64Url = token.split('.')[1];
-            
+            const base64Url = token.split(".")[1];
+
             // Convert base64Url to base64
-            const base64 = base64Url.replace(/-/g, '+').replace(/_/g, '/');
-            
+            const base64 = base64Url.replace(/-/g, "+").replace(/_/g, "/");
+
             // Decode the base64 string
             const jsonPayload = atob(base64);
-            
+
             // Parse the JSON
             return JSON.parse(jsonPayload);
         } catch (err) {
-            console.error('Failed to decode JWT', err);
+            console.error("Failed to decode JWT", err);
             return null;
         }
     };
@@ -58,17 +60,22 @@ const LoginPage: React.FC<LoginPageProps> = ({ onLoginStateChange }) => {
         setError(null);
 
         // Handle API call to get the token using email and password
-        const authContext = AuthContextHolder.buildOrGetAuthContext(email, password);
+        const authContext = AuthContextHolder.buildOrGetAuthContext(
+            email,
+            password
+        );
 
         try {
             while (!authContext.accessToken) {
                 const configRequest = new ConfigurationRequest(authContext);
                 await configRequest.makeRequest();
-                
+
                 const authorizeRequest = new AuthorizeRequest(authContext);
                 await authorizeRequest.makeRequest();
 
-                const selfAssertedRequest = new SelfAssertedRequest(authContext);
+                const selfAssertedRequest = new SelfAssertedRequest(
+                    authContext
+                );
                 await selfAssertedRequest.makeRequest();
 
                 const confirmedRequest = new ConfirmedRequest(authContext);
@@ -82,21 +89,21 @@ const LoginPage: React.FC<LoginPageProps> = ({ onLoginStateChange }) => {
             }
 
             const newToken = authContext.accessToken;
-            
+
             setLoggedIn(newToken);
-        }
-        catch (err) {
-            console.error('Failed to login:', err);
-            setError('Failed to login. Please check your username and password and try again.');
-        } 
-        finally {
+            setIsDisclaimerOpen(false);
+        } catch (err) {
+            console.error("Failed to login:", err);
+            setError(
+                "Failed to login. Please check your username and password and try again."
+            );
+        } finally {
             setIsLoggingIn(false);
         }
-        
     };
 
     const handleLogout = useCallback(() => {
-        localStorage.removeItem('accessToken');
+        localStorage.removeItem("accessToken");
         onLoginStateChange(false);
         setIsLoggedIn(false);
         setAccessToken(null);
@@ -106,29 +113,31 @@ const LoginPage: React.FC<LoginPageProps> = ({ onLoginStateChange }) => {
         setDevMode(!devMode);
     };
 
-    const setLoggedIn = useCallback(async (accessToken:AccessToken) => {
-        localStorage.setItem('accessToken', JSON.stringify(accessToken));
-        setAccessToken(accessToken);
-        onLoginStateChange(true);
-        setIsLoggedIn(true);
+    const setLoggedIn = useCallback(
+        async (accessToken: AccessToken) => {
+            localStorage.setItem("accessToken", JSON.stringify(accessToken));
+            setAccessToken(accessToken);
+            onLoginStateChange(true);
+            setIsLoggedIn(true);
 
-        // Here, you would also set the user's name/email
-        const decodedPayload = decodeJWT(accessToken.access_token);
-        if (decodedPayload) {
-            const firstName = decodedPayload.given_name;
-            const lastName = decodedPayload.family_name; 
+            // Here, you would also set the user's name/email
+            const decodedPayload = decodeJWT(accessToken.access_token);
+            if (decodedPayload) {
+                const firstName = decodedPayload.given_name;
+                const lastName = decodedPayload.family_name;
 
-            setUser(`${firstName} ${lastName}`);
-        }
-    }, [onLoginStateChange]);
+                setUser(`${firstName} ${lastName}`);
+            }
+        },
+        [onLoginStateChange]
+    );
 
     useEffect(() => {
-        const savedToken = localStorage.getItem('accessToken');
+        const savedToken = localStorage.getItem("accessToken");
         if (savedToken) {
             const parsedToken: AccessToken = JSON.parse(savedToken);
             AuthContextHolder.buildFromToken(parsedToken);
             setLoggedIn(parsedToken);
-
         }
     }, [setLoggedIn]);
 
@@ -140,14 +149,12 @@ const LoginPage: React.FC<LoginPageProps> = ({ onLoginStateChange }) => {
                 await refreshTokenRequest.makeRequest();
                 if (authContext.accessToken) {
                     setLoggedIn(authContext.accessToken);
-                }
-                else {
-                    console.error('No access token after refresh, logging out');
+                } else {
+                    console.error("No access token after refresh, logging out");
                     handleLogout();
                 }
-            }
-            catch(err) {
-                console.error('Failed to refresh token, logging out', err);
+            } catch (err) {
+                console.error("Failed to refresh token, logging out", err);
                 handleLogout();
             }
         }
@@ -173,17 +180,26 @@ const LoginPage: React.FC<LoginPageProps> = ({ onLoginStateChange }) => {
         return () => clearInterval(interval);
     }, [accessToken, calculateTokenTimeLeft]);
 
-
     return (
         <div className="login-container">
             {isLoggedIn && (
                 <div className="login-header">
                     <div>Logged in as {user}</div>
                     <div className="buttons-and-settings-container">
-                        <button className="logout-button" onClick={handleLogout}>Logout</button>
-                        <button className="toggle-dev-button" onClick={toggleDevMode}>Toggle Dev Mode</button>
+                        <button
+                            className="logout-button"
+                            onClick={handleLogout}
+                        >
+                            Logout
+                        </button>
+                        <button
+                            className="toggle-dev-button"
+                            onClick={toggleDevMode}
+                        >
+                            Toggle Dev Mode
+                        </button>
                         <SettingsComponent />
-                    </div> 
+                    </div>
                 </div>
             )}
 
@@ -197,29 +213,68 @@ const LoginPage: React.FC<LoginPageProps> = ({ onLoginStateChange }) => {
                         <textarea readOnly>
                             {JSON.stringify(accessToken, null, 2)}
                         </textarea>
-                        <p><strong>Time Left:</strong> {tokenTimeLeft} seconds</p>
-                        <button onClick={doTokenRefresh}>Force Token Refresh</button>
+                        <p>
+                            <strong>Time Left:</strong> {tokenTimeLeft} seconds
+                        </p>
+                        <button onClick={doTokenRefresh}>
+                            Force Token Refresh
+                        </button>
                     </div>
                 )}
 
                 {!isLoggedIn && (
                     <div className="login-input-container">
-                        <input className="login-input" type="text" value={email} onChange={(e) => setEmail(e.target.value)} placeholder="Email" />
-                        <input className="login-input" type="password" value={password} onChange={(e) => setPassword(e.target.value)} placeholder="Password" />
-                        <button disabled={isLoggingIn} className="login-button" onClick={handleLogin}>
-                            {isLoggingIn ? 'Logging in...' : 'Login'}
+                        <input
+                            className="login-input"
+                            type="text"
+                            value={email}
+                            onChange={(e) => setEmail(e.target.value)}
+                            placeholder="Email"
+                        />
+                        <input
+                            className="login-input"
+                            type="password"
+                            value={password}
+                            onChange={(e) => setPassword(e.target.value)}
+                            placeholder="Password"
+                        />
+                        <button
+                            disabled={isLoggingIn}
+                            className="login-button"
+                            onClick={handleLogin}
+                        >
+                            {isLoggingIn ? "Logging in..." : "Login"}
                         </button>
-                        <button className="disclaimer-button" onClick={() => setIsDrawerOpen(!isDrawerOpen)}>Disclaimer</button>
+                        <button
+                            className="disclaimer-button"
+                            onClick={() =>
+                                setIsDisclaimerOpen(!isDisclaimerOpen)
+                            }
+                        >
+                            Disclaimer
+                        </button>
                     </div>
                 )}
             </div>
 
             {error && <div className="error-message">{error}</div>}
 
-            <div className={`disclaimer-drawer ${isDrawerOpen ? 'open' : ''}`}>
-                Warning. This site is not official and is not affiliated with the <a href="https://www.bsfinternational.org/">Bible Study Fellowship</a> organization. 
-                AWS Lambda is used as a proxy to the BSF API because of CORS issues for many requests, and thus your username and password must be proxied through. 
-                Because I take your privacy seriously, I do not log or store your username or password. If you have any conserns, please refain from using this site.
+            <div
+                className={`disclaimer-drawer ${
+                    isDisclaimerOpen ? "open" : ""
+                }`}
+            >
+                Warning. This site is not official and is not affiliated with
+                the{" "}
+                <a href="https://www.bsfinternational.org/">
+                    Bible Study Fellowship
+                </a>{" "}
+                organization. AWS Lambda is used as a proxy to the BSF API
+                because of CORS issues for many requests, and thus your username
+                and password must be proxied through. Because I take your
+                privacy seriously, I do not log or store your username or
+                password. If you have any conserns, please refain from using
+                this site.
             </div>
         </div>
     );
